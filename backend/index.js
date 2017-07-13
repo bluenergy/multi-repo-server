165c5ecd8d73b02835f67e929d6cfdba5caf85b1
fnjoe <632771338@qq.com>
2017-07-13 23:48:57 +0800

    projects based on branch and an api for deploy status

--- backend/index.js
+++ backend/index.js
@@ -4,140 +4,243 @@ const { execInPromise } = require('./exec');
 const { workspacePath, projectPath } = require('./config');
 const { pathStatus, rmdirSync, copyDir } = require('./fs');
 
-function deployRepository(repository, address, branch, env, buildDirName) {
-    // 对应的项目目录
+function Repository(repository, address, branch, env, buildDirName) {
+    this.repository = repository;
+    this.address = address;
+    this.branch = branch;
+    this.env = env;
+    this.buildDirName = buildDirName;
+    this.status = '尚未开始';
+
+    this.init();
+}
+
+Repository.prototype.init = function() {
+    const repository = this.repository;
+    const address = this.address;
+    const branch = this.branch;
+    const env = this.env;
+    const buildDirName = this.buildDirName;
+
+    // 对应的项目主目录
     const workspaceProjectPath = path.resolve(__dirname, '../', './workspace/' + repository);
+    // 分支目录
+    const workspaceProjectBranchPath = path.resolve(workspaceProjectPath, `./${branch}`);
+    //  分支仓库目录
+    const workspaceProjectBranchGitPath = path.resolve(workspaceProjectBranchPath, `./${repository}`);
     // 项目的依赖目录
-    const workspaceProjectNodeModulesPath = path.resolve(__dirname, '../', './workspace/' + repository + '/node_modules');
+    const workspaceProjectNodeModulesPath = path.resolve(workspaceProjectBranchGitPath, './node_modules');
 
     pathStatus(workspacePath).then(
-        function (stat) {
+        (stat) => {
             if (stat.isDirectory()) {
+                //  根目录存在
                 pathStatus(workspaceProjectPath).then(
-                    function (stat) {
+                    (stat) => {
                         if (stat.isDirectory()) {
-                            //  项目目录存在
-                            pull(workspaceProjectPath).then(
-                                () => {
-                                    return checkBranch(workspaceProjectPath, branch);
-                                }
-                            ).then(
-                                () => {
-                                    return pathStatus(workspaceProjectNodeModulesPath).then(
-                                        function (stat) {
-                                            if (stat.isDirectory()) {
-                                                return build(workspaceProjectPath);
+                            //  项目主目录存在
+                            pathStatus(workspaceProjectBranchPath).then(
+                                (stat) => {
+                                    if (stat.isDirectory()) {
+                                        // 分支目录存在
+                                        this.pull(workspaceProjectBranchGitPath).then(
+                                            () => {
+                                                return this.checkBranch(workspaceProjectBranchGitPath, branch);
                                             }
-                                        },
-                                        function () {
-                                            return install(workspaceProjectPath).then(
+                                        ).then(
+                                            () => {
+                                                return pathStatus(workspaceProjectNodeModulesPath).then(
+                                                    (stat) => {
+                                                        if (stat.isDirectory()) {
+                                                            return this.build(workspaceProjectBranchGitPath);
+                                                        }
+                                                    },
+                                                    () =>{
+                                                        return this.install(workspaceProjectBranchGitPath).then(
+                                                            () => {
+                                                                return this.build(workspaceProjectBranchGitPath);
+                                                            }
+                                                        );
+                                                    }
+                                                )
+                                            }
+                                        ).then(
+                                            () => {
+                                                return this.deploy(workspaceProjectBranchGitPath, repository, branch, buildDirName);
+                                            }
+                                        )
+                                    }
+                                },
+                                ()  => {
+                                    //  项目目录存在，分支目录不存在
+                                    fs.mkdir(workspaceProjectBranchPath, (err) => {
+                                            if (err) {
+                                                throw err;
+                                            }
+                                            this.clone(workspaceProjectBranchPath, address).then(
                                                 () => {
-                                                    return build(workspaceProjectPath);
+                                                    return this.checkBranch(workspaceProjectBranchGitPath, branch);
                                                 }
-                                            );
+                                            ).then(
+                                                () => {
+                                                    return this.install(workspaceProjectBranchGitPath);
+                                                }
+                                            ).then(
+                                                () => {
+                                                    return this.build(workspaceProjectBranchGitPath);
+                                                }
+                                            ).then(
+                                                () => {
+                                                    return this.deploy(workspaceProjectBranchGitPath, repository, branch, buildDirName);
+                                                }
+                                            )
                                         }
-                                    )
-                                }
-                            ).then(
-                                () => {
-                                    return deploy(workspaceProjectPath, buildDirName);
+                                    );
                                 }
                             )
                         }
                     },
-                    function () {
+                    () => {
                         // 根目录存在，项目目录不存在
-                        clone(workspacePath, address).then(
-                            () => {
-                                return checkBranch(workspaceProjectPath, branch);
+                        fs.mkdir(workspaceProjectPath, (err) => {
+                            if (err) {
+                                throw err;
                             }
-                        ).then(
-                            () => {
-                                return install(workspaceProjectPath);
-                            }
-                        ).then(
-                            () => {
-                                return build(workspaceProjectPath);
-                            }
-                        ).then(
-                            () => {
-                                return deploy(workspaceProjectPath, buildDirName);
-                            }
-                        )
+                            fs.mkdir(workspaceProjectBranchPath, (err) => {
+                                if (err) {
+                                    throw err;
+                                }
+                                this.clone(workspaceProjectBranchPath, address).then(
+                                    () => {
+                                        return this.checkBranch(workspaceProjectBranchGitPath, branch);
+                                    }
+                                ).then(
+                                    () => {
+                                        return this.install(workspaceProjectBranchGitPath);
+                                    }
+                                ).then(
+                                    () => {
+                                        return this.build(workspaceProjectBranchGitPath);
+                                    }
+                                ).then(
+                                    () => {
+                                        return this.deploy(workspaceProjectBranchGitPath, repository, branch, buildDirName);
+                                    }
+                                )
+                            })
+                        })
                     }
                 )
             }
         },
-        function () {
+        () => {
             // 根目录不存在
-            fs.mkdir(workspacePath, function (err) {
+            fs.mkdir(workspacePath, (err) => {
                     if (err) {
                         throw err;
                     }
-                    clone(workspacePath, address).then(
-                        () => {
-                            return checkBranch(workspaceProjectPath, branch);
+                    fs.mkdir(workspaceProjectPath, (err) => {
+                        if (err) {
+                            throw err;
                         }
-                    ).then(
-                        () => {
-                            return install(workspaceProjectPath);
-                        }
-                    ).then(
-                        () => {
-                            return build(workspaceProjectPath);
-                        }
-                    ).then(
-                        () => {
-                            return deploy(workspaceProjectPath, buildDirName);
-                        }
-                    )
+                        fs.mkdir(workspaceProjectBranchPath, (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                            this.clone(workspaceProjectBranchPath, address).then(
+                                () => {
+                                    return this.checkBranch(workspaceProjectBranchGitPath, branch);
+                                }
+                            ).then(
+                                () => {
+                                    return this.install(workspaceProjectBranchGitPath);
+                                }
+                            ).then(
+                                () => {
+                                    return this.build(workspaceProjectBranchGitPath);
+                                }
+                            ).then(
+                                () => {
+                                    return this.deploy(workspaceProjectBranchGitPath, repository, branch, buildDirName);
+                                }
+                            )
+                        })
+                    })
                 }
             );
         }
     )
 }
 
-function clone(path, address) {
+Repository.prototype.clone = function(path, address) {
     console.log('开始clone项目');
+    this.status = '开始clone项目';
     return execInPromise(`cd ${path} && git clone ${address}`);
 }
-function checkBranch(path, branch) {
+Repository.prototype.checkBranch = function(path, branch) {
     console.log(`开始切换分支,切换到${branch}分支`);
+    this.status = `开始切换分支,切换到${branch}分支`;
     return execInPromise(`cd ${path} && git checkout ${branch}`);
 }
-function install(path) {
+Repository.prototype.install = function(path) {
     console.log('开始安装项目依赖');
+    this.status = '开始安装项目依赖';
     return execInPromise(`cd ${path} && npm install`);
 }
-function pull(path) {
+Repository.prototype.pull = function(path) {
     console.log('项目存在，开始更新项目');
+    this.status = '项目存在，开始更新项目';
     return execInPromise(`cd ${path} && git pull`);
 }
-function build(path, stage) {
+Repository.prototype.build = function(path, stage) {
     console.log('开始打包');
+    this.status = '开始打包';
     const stageArgue = stage ? `:${stage}` : '';
     return execInPromise(`cd ${path} && npm run build${stageArgue}`);
 }
-function deploy(workspaceProjectPath, buildDirName) {
+Repository.prototype.deploy = function(workspaceProjectBranchGitPath, repository, branch, buildDirName) {
     console.log('开始部署');
-
-    const buildSrcPath = path.resolve(workspaceProjectPath, `./${buildDirName}`);
-    const buildDistPath = path.resolve(projectPath, `./${buildDirName}`);
+    this.status = '开始部署';
+    const buildSrcPath = path.resolve(workspaceProjectBranchGitPath, `./${buildDirName}`);
+    const buildDistPath = path.resolve(projectPath, `./${repository}`);
+    const buildDistBranchPath = path.resolve(buildDistPath, `./${branch}`);
+    const buildDistDirPath = path.resolve(buildDistBranchPath, `./${buildDirName}`);
 
     return pathStatus(projectPath).then(
         function (stat) {
             if (stat.isDirectory()) {
+                //  服务地址存在
                 pathStatus(buildDistPath).then(
                     function (stat) {
                         if (stat.isDirectory()) {
-                            rmdirSync(buildDistPath, function(err) {
-                                if (err) throw err;
-                                copyDir(buildSrcPath, buildDistPath);
-                            })
+                            //  服务项目存在
+                            pathStatus(buildDistBranchPath).then(
+                                function (stat) {
+                                    if (stat.isDirectory()) {
+                                        rmdirSync(buildDistDirPath, function(err) {
+                                            if (err) throw err;
+                                            copyDir(buildSrcPath, buildDistDirPath);
+                                        })
+                                    }
+                                },
+                                function () {
+                                    fs.mkdir(buildDistBranchPath, function(err) {
+                                        if (err) throw err;
+                                        copyDir(buildSrcPath, buildDistDirPath);
+                                    })
+                                }
+                            );
                         }
                     },
                     function () {
-                        copyDir(buildSrcPath, buildDistPath);
+                        //  服务项目不存在
+                        fs.mkdir(buildDistPath, function(err) {
+                            if (err) throw err;
+                            fs.mkdir(buildDistBranchPath, function(err) {
+                                if (err) throw err;
+                                copyDir(buildSrcPath, buildDistDirPath);
+                            })
+                        })
                     }
                 );
             }
@@ -147,16 +250,23 @@ function deploy(workspaceProjectPath, buildDirName) {
                 if (err) {
                     console.log(err);
                 }
-                copyDir(buildSrcPath, buildDistPath);
+                fs.mkdir(buildDistPath, function(err) {
+                    if (err) throw err;
+                    fs.mkdir(buildDistBranchPath, function(err) {
+                        if (err) throw err;
+                        copyDir(buildSrcPath, buildDistDirPath);
+                    })
+                })
             })
         }
     ).then(
-        function () {
+        () => {
             console.log('部署成功');
+            this.status = '部署成功';
         }
     );
 }
 
 module.exports = {
-    deployRepository
+    Repository
 };
--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const scripts = require('./backend/index');
+const { Repository } = require('./backend/index');
+const exec = require('./backend/exec');
+
+const deployQueue = [];
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -15,11 +18,42 @@ app.get('/', function (req, res) {
 // 部署请求
 app.post('/deploy', function (req, res) {
     if (req.body.project === 'gulp-AngularJS1.x-seed') {
-        scripts.deployRepository('gulp-AngularJS1.x-seed' , 'https://github.com/hjzheng/gulp-AngularJS1.x-seed.git', req.body.branch, req.body.env, 'build');
-        res.status(200).end();
+    	const target = deployQueue.find(
+    		(repository) => {
+    			return repository.repository === 'gulp-AngularJS1.x-seed'
+    		}
+		);
+		console.log(target); 
+    	if (target && target.status !== '部署成功') {
+    		res.json({message: '正在部署，请稍后'});
+    	}
+    	if (target && target.status === '部署成功') {
+    		const index = deployQueue.findIndex(
+	    		(repository) => {
+	    			repository.repository === 'gulp-AngularJS1.x-seed'
+	    		}
+			);
+			deployQueue.splice(index, 1);
+    		deployQueue.push(new Repository('gulp-AngularJS1.x-seed' , 'https://github.com/hjzheng/gulp-AngularJS1.x-seed.git', req.body.branch, req.body.env, 'build'));
+			res.json({message: '上次已经部署成功，开始重新部署'});
+    	}
+    	if (!target) {
+    		deployQueue.push(new Repository('gulp-AngularJS1.x-seed' , 'https://github.com/hjzheng/gulp-AngularJS1.x-seed.git', req.body.branch, req.body.env, 'build'));
+			res.json({message: '开始重新部署'});
+    	}
     }
 });
-
+// 获取部署状态
+app.get('/deploy', function (req, res) {
+	res.json(deployQueue.map(
+		(repository) => {
+    		return {
+    			url: `localhost:9527/projects/${repository.repository}/${repository.branch}/${repository.buildDirName}`,
+    			status: repository.status
+    		}
+		}
+	));
+});
 app.listen(9527, function () {
     console.log('test app listening on port 9527!')
 });
